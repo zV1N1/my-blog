3f1867620dac03a7759eb32004d5d3b3ef6bfda5
zV1N1 <viniciusjose938@gmail.com>
2021-05-23 14:48:35 -0300

    'add-url-disqus'

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,10 +1,11 @@
-import { DiscussionEmbed } from 'disqus-react'
-import { Container } from './styled'
+import { DiscussionEmbed } from 'disqus-react';
+import { SITE_URL } from '../../config/app-config';
+import { Container } from './styled';
 
 export type CommentsProps = {
   slug: string;
-  title: string
-}
+  title: string;
+};
 
 export const Comments = ({ slug, title }: CommentsProps) => {
   return (
@@ -12,13 +13,12 @@ export const Comments = ({ slug, title }: CommentsProps) => {
       <DiscussionEmbed
         shortname="my-opinion-3"
         config={{
-          url: `/post/${slug}`,
+          url: `${SITE_URL}/post/${slug}`,
           identifier: slug,
           title: title,
-          language: 'pt_BR'
+          language: 'pt_BR',
         }}
       />
     </Container>
-  )
-}
-
+  );
+};
